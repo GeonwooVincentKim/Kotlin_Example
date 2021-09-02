7de64157b93230b004cc5338db2acd06fdf23216
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-02 16:21:29 +0900

    [Feature] - Type Casting

---
+++ Kotlin_Grammer/02.kt
@@ -0,0 +1,8 @@
+fun main() {
+    // Type Casting - Explicit Type Casting
+    // --> Explicit Type Casting(Forced Casting)
+    // --> Implicit Type Casting(Auto Casting)
+    var a: Int = 54321
+    var b: Long = a.toLong()
+    println("a: $a\nb: $b");
+}
