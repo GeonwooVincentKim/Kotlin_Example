c54305c16217ebae0a077911ed7200ef3c492522
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-02 16:43:40 +0900

    [Feature] - Int Array and Null Array

--- Kotlin_Grammer/02.kt
+++ Kotlin_Grammer/02.kt
@@ -4,5 +4,33 @@ fun main() {
     // --> Implicit Type Casting(Auto Casting)
     var a: Int = 54321
     var b: Long = a.toLong()
-    println("a: $a\nb: $b");
+    println("a: $a\nb: $b\n");
+
+    // Array - Have owns values
+    // var intOldArr = {1, 2, 3, 4, 5} --> Could occurs error because this is not a array in Kotlin
+    var intArr = arrayOf(1, 2, 3, 4, 5);
+    println("intArr: $intArr");
+
+    for(i in intArr){
+        println(i);
+
+        if(i == intArr.size){
+            println("$i\n")
+        }
+    }
+
+    // Array - Initialize Array
+    var nullArr = arrayOfNulls<Int>(5)
+    println("nullArr: $nullArr");
+
+    intArr[2] = 8
+    println("intArr[4]: " + intArr[4] + "\n");
+
+    for(i in intArr){
+        println(i);
+
+        if(i == intArr.size){
+            println("$i\n");
+        }
+    }
 }
