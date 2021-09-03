8dfe540817d1cfd5c1a7ba22285348e36790e747
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-03 14:30:09 +0900

    [Feature] - Implement Constructor overloading

--- Kotlin_Grammer/06.kt
+++ Kotlin_Grammer/06.kt
@@ -3,9 +3,19 @@ fun main(){
     var b = Person("전정국", 1997)
     var c = Person("홍길동", 2003)
 
-    a.introduce();
-    b.introduce();
-    c.introduce();
+    println("a: $a - b: $b - c: $c");
+    println();
+
+    var d = Person("이루다")
+    var e = Person("차은우")
+    var f = Person("류수정")
+
+    println("d: $d - e: $e - f: $f");
+    println();
+
+    // a.introduce();
+    // b.introduce();
+    // c.introduce();
 }
 
 /* Use Class-Attribute Only */
@@ -13,7 +23,21 @@ fun main(){
 
 /* Create Class-Function */
 class Person(var name: String, val birthYear: Int){
-    fun introduce(){
-        println("안녕하세요, ${birthYear}년생 ${name}입니다.");
+    /* Basic Constructor(Initializer) - Declare when create `Class` */
+    init {
+        println("${this.birthYear}년생 ${this.name}님이 생성되었습니다.");
     }
+
+    /* 
+        Secondary Constructor(Initialzer) 
+        1. Declare features(or property) addtionally when need it 
+        2. Initializer Overloading
+    */
+    constructor(name: String) : this(name, 1997){
+        println("보조 생성자가 사용되었습니다.")
+    }
+
+    // fun introduce(){
+    //     println("안녕하세요, ${birthYear}년생 ${name}입니다.");
+    // }
 }
