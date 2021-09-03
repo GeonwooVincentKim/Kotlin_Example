00ef33cf5598dbb9405a029f0134b642cb27ef19
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-03 16:17:12 +0900

    [Feature] - Testing

--- Hello.kt
+++ Hello.kt
@@ -1,6 +1,6 @@
 package com.kotlintest.Kotlin_Grammer
 
-import com.kotlintest.Kotlin_Grammer.Runner
+import com.kotlintest.Kotlin_Grammer.main
 
 // Press `Ctrl + alt + n` to execute program
 fun greeting(){
