3fa8deca253c752d8b9bf5e1a77ac7976efdb175
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-03 11:34:20 +0900

    [Feature] - Do~While

--- Kotlin_Grammer/05.kt
+++ Kotlin_Grammer/05.kt
@@ -11,4 +11,18 @@ fun main(){
     while(a < 5){
         println(++a);       
     }
+
+    println();
+    a = 0
+    
+    do{
+        println(a++)
+    } while(a < 5)
+
+    println();
+    a = 0
+
+    do{
+        println(++a)
+    } while (a < 5)
 }
