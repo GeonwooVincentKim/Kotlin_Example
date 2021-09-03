7739a0cd1c6ffd5ff4f78df5ceede24cab39c534
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-03 11:14:44 +0900

    [Feature] - Return values in `When`

--- Kotlin_Grammer/04.kt
+++ Kotlin_Grammer/04.kt
@@ -18,6 +18,7 @@ fun main(){
         println(false);
     }
 
+    println();
     doWhen(1);
     doWhen("String");
     doWhen(12L);
@@ -28,7 +29,11 @@ fun main(){
 
 // Any: Allowed all of type of Kotlin Programming Language
 fun doWhen(a: Any){
-    when(a){
+    /*
+        When you just want to show what you've texted
+        (Which mean without `Return` behaviour)
+    */
+    /*when(a){
         1 -> println("정수 1입니다")
         "String" -> println("문자열입니다.")
         
@@ -36,5 +41,20 @@ fun doWhen(a: Any){
         !is String -> println("String 입니다")
 
         else -> println("어떤 조건도 만족하지 않습니다")
+    }*/
+
+    /*
+        When you want to `Return` some particular values
+    */
+    var result = when(a){
+        1 ->"정수 1입니다"
+        "String" -> "문자열입니다."
+        
+        is Long -> "Long Type 변수입니다"
+        !is String -> "String 입니다"
+
+        else -> "어떤 조건도 만족하지 않습니다"
     }
+
+    println(result);
 }
