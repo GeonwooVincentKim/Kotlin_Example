de6408ff7b3c2d4ba3951a726be296a5c75ebd41
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-03 09:53:56 +0900

    [Feature] - Type Interface and lots of way to use Function in Kotlin

--- Kotlin_Grammer/03.kt
+++ Kotlin_Grammer/03.kt
@@ -28,14 +28,38 @@ fun main(){
         println("ayo: $ayo\n");
         println(printStringValue);
     }
+
+    var number1: Int = 10
+    var number2: Int = 20
+    var number3: Int = 30
+
+    var sum: Int = addNumber(number1, number2, number3);
+    println("sum: $sum");
+
+    var sumNew: Int = addNumberShort(number1, number2, number3);
+    println("sumNew: $sumNew");
 }
 
+// Without Return Type --> void Type
 // fun printTypeInterface(a: Int, b: Long, c: Double, d: Float, e: Int, f: Int, g: Boolean, h: Char){
 //     println("a: $a\nb: $b\nc: $c\nd: $d\ne: $e\nf: $f\ng: $g\nh: $h");
 //     println();
 // }
 
+// With Return Type --> String Type
 fun printTypeInterface(a: Int, b: Long, c: Double, d: Float, e: Int, f: Int, g: Boolean, h: Char): String{
     var stringValue: String = "a: $a\nb: $b\nc: $c\nd: $d\ne: $e\nf: $f\ng: $g\nh: $h";
     return stringValue;
 }
+
+// General Function --> This function have several features
+fun addNumber(a: Int, b: Int, c: Int): Int{
+    var sum: Int = a + b + c;
+    return sum;
+}
+
+/* Simple Express Function
+    1. This function has single-feature only, not have several features
+    2. Skip determine `Return` Type
+*/
+fun addNumberShort(a: Int, b: Int, c: Int) = a + b + c;
