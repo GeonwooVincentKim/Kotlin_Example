a0222415fa6c0409854fd487f8e6b6521c50f68c
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-03 15:08:07 +0900

    [Feature] - Inheritance

--- Kotlin_Grammer/07.kt
+++ Kotlin_Grammer/07.kt
@@ -1,9 +1,27 @@
 fun main(){
-
+    var a = Animal("별이", 5, "개");
+    var b = Dog("별이", 5);
+    
+    a.introduce();
+    b.introduce();
 }
 
-class Animal(var name: String, var age: Int, var type: String){
+/* Parent Class (Super Class) */
+open class Animal(var name: String, var age: Int, var type: String){
     fun introduce(){
         println("저는 ${type} ${name}이고, ${age}살 입니다.");
     }
+}
+
+/* 
+    Child Class (Sub Class) 
+    -> In java Code
+    public class Dog{
+        public Dog(String name, int age, String type){
+            super(name, age, type);
+        }
+    }
+*/
+class Dog(name: String, age: Int) : Animal(name, age, "개"){
+    
 }
