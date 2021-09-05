b4cc7f4f9135159fb3ca31ff05bde40f5c7ded2c
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-05 21:46:39 +0900

    [Feature] - Lambda Function

--- Kotlin_Grammer/11.kt
+++ Kotlin_Grammer/11.kt
@@ -11,4 +11,37 @@ fun main(){
 
     println("calculate result : $calculate")
     // println("a result : $a")
+
+    /*
+        Original Way to use Instance of Class
+
+        var a = Book("Contents", 10000)
+        a.name = "[초특가]" + a.name
+        a.discount()
+    */
+    var a = Book("Contents", 10000)
+        a.name = "[초특가]" + a.name
+        a.discount()
+
+    a.run{
+        println("No Apply Scope Function")
+        println("상품명 : ${name}, 가격 ${price}원")
+    }
+        
+    println()
+    var applyInstance = Book("Demo's Kotlin", 10000).apply{
+        name = "[초특가]" + name
+        discount()
+    }
+
+    applyInstance.run{
+        println("With Apply Scope Function")
+        println("상품명 : ${name}, 가격 ${price}원")
+    }
+}
+
+class Book(var name: String, var price: Int){
+    fun discount(){
+        price -= 2000
+    }
 }
