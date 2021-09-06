64f2e0a6c4419c93d643f325d59dffb0ecc7bdce
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-06 15:44:50 +0900

    [Feature] - Handle data with list

--- Kotlin_Grammer/24.kt
+++ Kotlin_Grammer/24.kt
@@ -11,4 +11,20 @@ fun main(){
     val (over98, under98) = personList.partition { it.birthYear > 1998 }
     println(over98)
     println(under98)
+
+    println()
+    println("-------------------")
+    println()
+
+    /* 
+        If the data that the users looking forward not exist using `Index`,
+        then insert 50 instead
+    */
+    val numbers = listOf(-3, 7, 2, -10, 1)
+    println(numbers.flatMap{listOf(it * 10, it + 10)})
+    println(numbers.getOrElse(1) {50})
+    println(numbers.getOrElse(10){50})
+
+    val names = listOf("A", "B", "C", "D")
+    println(names zip numbers)
 }
