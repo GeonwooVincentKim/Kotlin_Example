a9dd6f477589d6140f32e475548a137ee668a08b
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-06 15:05:17 +0900

    [Feature] - Map and Set

--- Kotlin_Grammer/22.kt
+++ Kotlin_Grammer/22.kt
@@ -1,4 +1,9 @@
 fun main(){
+    /*
+        MutableSet ->
+        1. Can insert or delete data easy
+        2. Could only insert Data(Value, but it operates like a key, like a Java Set)
+    */
     val a = mutableSetOf("귤", "바나나", "키위")
 
     for(item in a){
@@ -12,4 +17,21 @@ fun main(){
     println(a)
 
     println(a.contains("귤"))
+    println()
+
+    /* 
+        MutableMap ->
+        1. Can insert or delete data easy
+        2. Connect `Key` and `Value` with `to`
+     */
+    val b = mutableMapOf("레드벨벳" to "음파음파", 
+                         "트와이스" to "FANCY", 
+                         "ITZY" to "ICY")
+
+    for(entry in b){
+        println("${entry.key} : ${entry.value}")
+    }
+
+    b.put("오마이걸", "번지")
+    println(b)
 }
