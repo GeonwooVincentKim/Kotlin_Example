75edc0515c984754fe34672f81dc30bdce99e734
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-06 10:55:22 +0900

    [Feature] - List for fruit

---
+++ Kotlin_Grammer/16.kt
@@ -0,0 +1,23 @@
+fun main(){
+    val a = listOf("Apple", "Strawberry", "Pear")
+    println(a[1])
+
+    for(fruit in a){
+        if(a.size == fruit.length - 1){
+            print("${fruit}")
+        } else {
+            print("${fruit} : ")
+        }
+    }
+    
+    println()
+
+    val b = mutableListOf(6, 3, 1)
+    println(b)
+
+    b.add(4)
+    println(b)
+
+    b.add(2, 8)
+    println(b)
+}
