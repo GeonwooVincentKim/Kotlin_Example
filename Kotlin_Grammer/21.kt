68f8aa03185b774e7052ce9b14282c7d70a37004
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-06 14:42:46 +0900

    [Feature] - Add Explanation

--- Kotlin_Grammer/21.kt
+++ Kotlin_Grammer/21.kt
@@ -15,7 +15,20 @@ fun main(){
     println(b.copy())
     println(b.copy("아린"))
     println(b.copy(id=618))
+
+    println()
+
+    var list = listOf(Data("보영", 212), Data("루다", 306), Data("아린", 618))
+    for((c, d) in list){
+        println("${c}, ${d}")
+    }
 }
 
 class General(val name: String, val id: Int)
+
+/* 
+    Data Class is like a `String` Class in Java. 
+    It inheritate from Object and Overrides what the `String` Class needs.
+    The Data Class is also similar with `String` in Kotlin
+*/
 data class Data(val name: String, val id: Int)
