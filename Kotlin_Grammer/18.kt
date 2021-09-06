5e1e2210115aca7112bd1f246aacfa318217dd07
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-06 13:46:56 +0900

    [Feature] - Equality of Object and Contents

--- Kotlin_Grammer/18.kt
+++ Kotlin_Grammer/18.kt
@@ -10,4 +10,29 @@ fun main(){
         println(uppercase())
         println(lowercase())
     }
+
+    var b = Product("콜라", 1000)
+    var c = Product("콜라", 1000)
+    
+    var d = b
+    var e = Product("사이다", 1000)
+
+    println(b == c)
+    println(b === c)
+
+    println(b == d)
+    println(b === d)
+
+    println(b == e)
+    println(b === e)
+}
+
+class Product(val name: String, val price: Int){
+    override fun equals(other: Any?): Boolean{
+        if(other is Product){
+            return other.name == name && other.price == price
+        } else {
+            return false
+        }
+    }
 }
