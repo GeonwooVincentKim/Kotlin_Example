7fca2b1cd339df1be9fca082285ab5ae7bbaab05
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-06 10:25:31 +0900

    [Feature] - Polymorphism Code of Kotlin

--- Kotlin_Grammer/14.kt
+++ Kotlin_Grammer/14.kt
@@ -1,10 +1,27 @@
 fun main(){
     var a = Drink()
     a.drink()
+
+    var b: Drink = Cola()
+    b.drink()
+
+    /*
+        is -> Downcast the type of instance when it locates in `if`
+        as -> 
+        1. Downcast the type of instance when declares the variable
+        2. Not only `Return Type` but also `variable` downcast
+    */
+    if (b is Cola){
+        b.washDishes()
+    }
+
+    var c = b as Cola
+    c.washDishes()
+    b.washDishes()
 }
 
 open class Drink{
-    var name = "Some Drink"
+    var name = "Drinks"
 
     open fun drink(){
         println("Drink ${name}")
@@ -15,7 +32,7 @@ class Cola: Drink(){
     var type = "Coke"
 
     override fun drink(){
-        println("Drink ${type} in ${name}")
+        println("Drink ${type} in the menu of ${name}")
     }
 
     fun washDishes(){
