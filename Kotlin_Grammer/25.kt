150f92a58c6a0e1faf4234caf806cc99624c92bd
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-06 16:01:40 +0900

    [Feature] - LateInit variables and data

--- Kotlin_Grammer/25.kt
+++ Kotlin_Grammer/25.kt
@@ -3,6 +3,16 @@ fun main(){
     foodCourt.searchPrice(FoodCourt.FOOD_CREAM_PASTA)
     foodCourt.searchPrice(FoodCourt.FOOD_STEAK)
     foodCourt.searchPrice(FoodCourt.FOOD_PIZZA)
+
+    println()
+    println("-------------------------")
+    println()
+
+    val a = LateInitSample()
+    
+    println(a.getLateInitText())
+    a.text = "A value that reallocated newly"
+    println(a.getLateInitText())
 }
 
 class FoodCourt(){
@@ -21,5 +31,21 @@ class FoodCourt(){
         const val FOOD_CREAM_PASTA = "Cream Pasta"
         const val FOOD_STEAK = "Steak"
         const val FOOD_PIZZA = "Pizza"
+    }
+}
+
+/* 
+    LateInit -> 
+    1. Determine the type of variable and allocates Object later
+    2. Cannot use variables until allocates `Initialize Value`
+*/
+class LateInitSample{
+    lateinit var text: String
+    fun getLateInitText(): String{
+        if(::text.isInitialized){
+            return text
+        } else {
+            return "Default Value"
+        }
     }
 }
