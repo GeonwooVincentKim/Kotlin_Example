2e257f1425b9be876926eaf519fef22fd92f6493
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-06 09:46:06 +0900

    [Feature] - Kotlin Observer Pattern Codes

---
+++ Kotlin_Grammer/13.kt
@@ -0,0 +1,26 @@
+fun main(){
+
+}
+
+interface EventListener{
+    fun onEvent(count: Int)
+}
+
+class Counter(var listener: EventListener){
+    fun count(){
+        for(i in 1..100){
+            if(i% 5 == 10) listener.onEvent(i)
+        }
+    }
+}
+
+class EventPrinter: EventListener{
+    override fun onEvent(count: Int){
+        print("${count} - ")
+    }
+
+    fun start(){
+        val counter = Counter(this)
+        counter.count()
+    }
+}
