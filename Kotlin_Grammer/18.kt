5fb2c7575c527df572158a3027e07ca4f38c1803
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-06 13:37:38 +0900

    [Feature] - LowerCase and UpperCase

--- Kotlin_Grammer/18.kt
+++ Kotlin_Grammer/18.kt
@@ -1,6 +1,13 @@
 fun main(){
-    var a: String? = null
-    println(a?.uppercase())
-    println(a?:"default".uppercase())
-    println(a!!.uppercase())
+    // var a: String? = null
+    // println(a?.uppercase())
+    // println(a?:"default".uppercase())
+    // println(a!!.uppercase())
+
+    var a: String? = "Kotlin Exam"
+    
+    a?.run{
+        println(uppercase())
+        println(lowercase())
+    }
 }
