90faa7973a4bc2b3e023967ac6d649dd02e0cae0
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-06 14:15:50 +0900

    [Feature] - Kotlin Function

--- Kotlin_Grammer/19.kt
+++ Kotlin_Grammer/19.kt
@@ -9,6 +9,23 @@ fun main(){
     deliveryItem("짬뽕")
     deliveryItem("책", 3)
     deliveryItem("노트북", 30, "학교")
+
+    /* Display the results without 2nd Argument */
+    deliveryItem("선물", destination="친구집")
+
+    println()
+    println("------------------")
+
+    /* Set the number of arguments by input the data in the arguments of `Sum` function */
+    sum(1, 2, 3, 4)
+    
+    println()
+    println("------------------")
+    
+    println(6 multiply 4)
+    println(6 multiply (4))
+
+    println(20.3 plus 128.4)
 }
 
 fun read(x: Int){
@@ -22,3 +39,18 @@ fun read(x: String){
 fun deliveryItem(name: String, count: Int = 1, destination: String = "집"){
     println("${name}, ${count}개를 ${destination}에 배달하였습니다.")
 }
+
+/* The number of arguments has not set yet */
+fun sum(vararg numbers: Int){
+    var sum = 0
+
+    for(n in numbers){
+        sum += n
+    }
+
+    print(sum)
+}
+
+/* Use like an Operator */
+infix fun Int.multiply(x: Int) : Int = this * x
+infix fun Double.plus(x: Double) : Double = this + x
